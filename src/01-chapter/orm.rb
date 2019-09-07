08c76ad7a046ccc9d992027b7819cf5cce9f9630
afeiship <1290657123@qq.com>
2019-09-07 22:31:58 +0800

    add: orm exmaple

--- src/01-chapter/orm.rb
+++ src/01-chapter/orm.rb
@@ -16,3 +16,30 @@ class Entity
     Database.sql("select #{col} from #{@table} where id=#{@ident}")[0][0]
   end
 end
+
+class Movie < Entity
+  def initialize(ident)
+    super "movies", ident
+  end
+
+  def title
+    get "title"
+  end
+
+  def title=(value)
+    set "title", value
+  end
+
+  def director
+    get "director"
+  end
+
+  def director=(value)
+    set "director", value
+  end
+end
+
+# 实际上，有个东西叫 ActiveRecord 做得更加的完美，这个实现有很多的冗余代码
+movie = Movie.new
+movie.title = "Doctor Stranglelove"
+movie.director = "Stanley Kubrik"
