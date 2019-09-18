6fa4f92e120c68e900d026522fbedf58c5db7e2f
afeiship <1290657123@qq.com>
2019-09-18 22:23:41 +0800

    add: opt example

--- src/04-chapter/015-proc-vs-lamda-arity.rb
+++ src/04-chapter/015-proc-vs-lamda-arity.rb
@@ -1,17 +1,17 @@
 p1 = proc do |n1, n2|
-  puts n1, n2
+  "inner #{n1},#{n2}"
 end
 
 p2 = lambda do |n1, n2|
-  puts n1, n2
+  "inner #{n1},#{n2}"
 end
 
-puts "args length:", p1.arity, p2.arity
+# puts "args length:", p1.arity, p2.arity
 
 ## proc will get nil args
-puts p1.call(1, 2, 3)
-puts p1.call(1, 2)
-puts p1.call(1)
+puts "3 args: #{p1.call(1, 2, 3)}"
+puts "2 args: #{p1.call(1, 2)}"
+puts "1 args: #{p1.call(1)}"
 
 ## `lambda` will caught error
 # puts p2.call(1, 2)
